65387cf54341f0b987f566330a4e5a2078312604
Cao Thiện Thiên Tân <alxarrow123@gmail.com>
2023-03-13 21:16:59 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,22 @@ builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlSer
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddCors(options =>
+    {
+
+        options.AddDefaultPolicy(
+            policy =>
+            {
+                policy.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+    });
+}
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,5 +42,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//CORs
+app.UseCors();
 
 app.Run();
