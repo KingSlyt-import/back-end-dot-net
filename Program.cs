a88c8a2b6902e1a5eecf1543d5f667778d7d2d84
Quan Nguyen <kingslyt36@gmail.com>
2023-04-07 11:02:44 +0700

    Upgrade project from .NET 6.0 -> .NET 7.0

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+// Add database connection string
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     // builder.Configuration.GetConnectionString("OnlineConnection")
 ));
+
+// Add JSON serialization for using HTTP PATCH
+builder.Services.AddControllers().AddNewtonsoftJson();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
